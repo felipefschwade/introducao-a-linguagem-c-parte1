2317d51df726bb8ab8386df801da6ef07c7ed071
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-09-30 12:06:24 -0400

    Acompanhamento da video aula 3

---
+++ adivinhacao.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	printf("******************************************\n");
+	printf("* Bem Vindo ao nosso jogo de adivinhacao *\n");
+	printf("******************************************\n");
+}
