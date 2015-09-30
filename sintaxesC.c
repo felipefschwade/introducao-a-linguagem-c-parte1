b7bb65d151edef9fd25f6f9a61c9e5960c3627ba
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-09-29 23:18:50 -0400

    Video Aula 2

---
+++ sintaxesC.c
@@ -0,0 +1,6 @@
+#include <stdio.h> -> Utilizado para incluir as bibliotecas da linguagem C
+int main() {/*Code*/} -> Função necessária para escrever programas em C
+printf("%s\n"); -> Para imprimir algo na tela. 
+
+/*Compilador GCC*/
+gcc programa.c -o programa.extensão
