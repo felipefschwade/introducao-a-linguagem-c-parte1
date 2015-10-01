38fdf67033ca2202adaa12686e0459b41aeb5a95
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-09-30 23:46:51 -0400

    Acompanhamento da video aula 5

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
 int main(){
 	printf("******************************************\n");
 	printf("* Bem Vindo ao nosso jogo de adivinhacao *\n");
 	printf("******************************************\n");
+
+	int numerosecreto = 42;
+	int chute;
+
+	printf("Qual e o seu chute? ");
+	scanf("%d", &chute);
+	printf("Seu chute foi %d\n", chute);
 }
