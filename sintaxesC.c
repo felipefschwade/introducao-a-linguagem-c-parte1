56f7dfc92779168eaf6c6a0ec1f116273f67ac2a
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-09-30 23:47:00 -0400

    Acompanhamento da video aula 5

--- sintaxesC.c
+++ sintaxesC.c
@@ -1,6 +1,8 @@
 #include <stdio.h> -> Utilizado para incluir as bibliotecas da linguagem C
 int main() {/*Code*/} -> Função necessária para escrever programas em C
 printf("%s\n"); -> Para imprimir algo na tela. 
-
+tipo nomevariavel = valor -> Para Definir variáveis em C
+printf("%d", var) -> Para imprimir variáveis do tipo inteiro;
+scanf("%tipo", &variavel) -> Para realizar leituras do teclado; 
 /*Compilador GCC*/
 gcc programa.c -o programa.extensão
