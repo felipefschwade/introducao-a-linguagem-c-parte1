4c01bb83e79221867b65b67b38b7374a0ba31c31
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-10-01 23:06:06 -0400

    Acompanhamento da Video Aula 3 - Modificando as verificações

--- adivinhacao.c
+++ adivinhacao.c
@@ -12,11 +12,13 @@ int main(){
 	scanf("%d", &chute);
 	printf("Seu chute foi %d\n", chute);
 
-	if (chute == numerosecreto){
+	int acertou = (chute == numerosecreto);	
+	if (acertou){
 		printf("Parabéns! Você acertou!\n");
 		printf("Jogue de novo, você é um bom jogador!\n");
 	} else {
-		if (chute > numerosecreto){
+		maior = (chute > numerosecreto);
+		if (maior){
 			printf("Seu chute foi maior que o número secreto.\n");
 		} else {
 			printf("Seu chute foi menor que o número secreto.\n");
