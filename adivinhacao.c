31421145e9b23f2208cbadb6b7b3e74ff1e1199f
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-10-02 12:37:42 -0400

    Acompanhamento da Video Aula 5 - Criando o laço FOR

--- adivinhacao.c
+++ adivinhacao.c
@@ -8,20 +8,25 @@ int main(){
 	int numerosecreto = 42;
 	int chute;
 
-	printf("Qual e o seu chute? ");
-	scanf("%d", &chute);
-	printf("Seu chute foi %d\n", chute);
+	for (int i = 0; i <= 3; ++i){
 
-	int acertou = (chute == numerosecreto);	
-	if (acertou){
-		printf("Parabéns! Você acertou!\n");
-		printf("Jogue de novo, você é um bom jogador!\n");
-	} else {
-		maior = (chute > numerosecreto);
-		if (maior){
-			printf("Seu chute foi maior que o número secreto.\n");
+		printf("Qual e o seu chute? ");
+		scanf("%d", &chute);
+		printf("Seu chute foi %d\n", chute);
+
+		int acertou = (chute == numerosecreto);	
+		if (acertou){
+			printf("Parabéns! Você acertou!\n");
+			printf("Jogue de novo, você é um bom jogador!\n");
 		} else {
-			printf("Seu chute foi menor que o número secreto.\n");
+			maior = (chute > numerosecreto);
+			if (maior){
+				printf("Seu chute foi maior que o número secreto.\n");
+			} else {
+				printf("Seu chute foi menor que o número secreto.\n");
+			}
 		}
+
 	}
+	
 }
