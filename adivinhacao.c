62835c13bff910094685bd11180a398e64b86f8b
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-10-01 22:44:18 -0400

    Acompanhamento da Video Aula 2

--- adivinhacao.c
+++ adivinhacao.c
@@ -11,4 +11,12 @@ int main(){
 	printf("Qual e o seu chute? ");
 	scanf("%d", &chute);
 	printf("Seu chute foi %d\n", chute);
+
+	if (chute == numerosecreto){
+		printf("Parabéns! Você acertou!\n");
+		printf("Jogue de novo, você é um bom jogador!\n");
+	} else {
+		printf("Você erro!\n");
+		printf("Mas não desanime, tente de novo!\n");
+	}
 }
