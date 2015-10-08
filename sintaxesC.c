3563a8b85d1c1ba6d7689c2a086d65f42fff93a4
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2015-10-08 11:52:04 -0400

    Definindo qual siglas utilizar para determinados tipos de variáveis

--- sintaxesC.c
+++ sintaxesC.c
@@ -7,7 +7,7 @@ int main() {/*Code*/} -> Função necessária para escrever programas em C
 printf("%s\n"); -> Para imprimir algo na tela. 
 tipo nomevariavel = valor -> Para Definir variáveis em C
 printf("%d", var) -> Para imprimir variáveis do tipo inteiro;
-scanf("%tipo", &variavel) -> Para realizar leituras do teclado; 
+scanf("%tipo", &variavel) -> Para realizar leituras do teclado (%d para int, %u para decimal sem sinal, %l para long int, %f para float, %lf para double); 
 abs(numero) -> Para Conseguir o número absoluto de um número;
 int num = rand() -> Para criar números aleatórios;
 num % numero -> Para gerar restos de divisões entre 0 e numero;
